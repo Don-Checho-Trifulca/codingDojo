d69f533ab7d727b607d69d0a9ee6a75a1dc16fcc
Benjamin160417 <benjamin.silva.osorio@alumnos.sip.cl>
2022-04-12 11:13:00 -0400

    cambio 3

---
+++ diagrama.js
@@ -0,0 +1,30 @@
+//fragmento de codigo 1
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+/*
+variable                     datos
+auntContactInfo     =    ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345]
+*/
+//fragmento de codigo 2
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+/*
+variable                     datos
+produce         =          ["manzanas", "naranjas"];
+frozen          =          ["brócoli", "helado"];
+*/
+//fragmento de codigo 3 
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+/*
+variable                     datos
+movieLibrary   =           ["Bambi", "E.T.", "Toy Story"];
+*/
